602167aede99d5200a71b868f143e27f6255092a
Shashwat Sethi <60094679+CoderGuardian@users.noreply.github.com>
2022-08-24 17:49:11 +0530

    Update Asgn2.py

--- Asgn2.py
+++ Asgn2.py
@@ -1,30 +1,25 @@
 def add(x, y):
     return x + y
-
 def subtract(x, y):
     return x - y
-
 def multiply(x, y):
     return x * y
-
 def divide(x, y):
     return x / y
-
-while True: 
+flag='Y'
+while flag == "Y" or flag == "y": 
     num1 = float(input("Enter first number: "))
     exp=input("Enter Operation : ")
     num2 = float(input("Enter second number: "))
  
-    if exp == '+':
-        print(num1, "+", num2, "=", add(num1, num2))
-    elif exp == '-':
-        print(num1, "-", num2, "=", subtract(num1, num2))
-    elif exp == '*':
-        print(num1, "x", num2, "=", multiply(num1, num2))
-    elif exp == '/':
-        print(num1, "/", num2, "=", divide(num1, num2))
+    if exp=='+':
+        print(num1,"+", num2,"=",add(num1, num2))
+    elif exp=='-':
+        print(num1,"-",num2,"=",subtract(num1, num2))
+    elif exp=='*':
+        print(num1,"x",num2,"=",multiply(num1, num2))
+    elif exp=='/':
+        print(num1,"/",num2,"=", divide(num1, num2))
     else:
-        print("Invalid Input")
-    flag = input("Exit? (Y/N): ")
-    if flag == "Y" or flag == "y":
-        break    
+        print("Wrong Operation")
+    flag = input("Exit? (Y/N): ")   
